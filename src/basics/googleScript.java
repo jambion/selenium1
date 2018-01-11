6c96998ff59e11951205a4889a13480d0868dbef
JXA <Ambion_J@yahoo.com>
2018-01-10 16:04:00 -0800

    Added nike script

--- src/basics/googleScript.java
+++ src/basics/googleScript.java
@@ -13,8 +13,12 @@ public class googleScript {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		
-		driver.findElement(By.xpath("//div[@class='lst-c']/div/div[2]/div/input")).sendKeys("Hi!!");
+//		driver.findElement(By.xpath("//div[@class='lst-c']/div/div[2]/div/input")).sendKeys("Hi!!");
 		// Search Bar XPATH: //div[@class='lst-c']/div/div[2]/div/input
+		////*[@id="gbw"]/div/div/div[1]/div[3]/a
+//		driver.findElement(By.xpath("//a[@data-pid='2']")).click();
+//		driver.findElement(By.cssSelector("a[data-pid='23']")).click();
+		driver.findElement(By.xpath("//div[@id='gb']/div[2]/div/div/div/div[1]/a")).click();
 		
 		
 
--- src/basics/nikeScript.java
+++ src/basics/nikeScript.java
@@ -6,16 +6,36 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class nikeScript {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver_win32//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.nike.com/t/air-vapormax-flyknit-utility-unisex-running-shoe-XPTbVZzp");
+		driver.get("https://store.nike.com/us/en_us/pw/nikelab-shoes/ofoZoi3");
 		
+		Thread.sleep(5000);
+		
+		driver.findElement(By.xpath("//*[text()='NikeLab Veil Gyakusou']")).click();
 		
 		//<input type="radio" id="skuAndSize__20823134:M 10 / W 11.5" name="skuAndSize" value="20823134:M 10 / W 11.5" aria-label="M 10 / W 11.5">
-		driver.findElement(By.xpath("//input[@id='skuAndSize__20823134:M 10 / W 11.5']")).click();
+//		driver.findElement(By.xpath("//input[@id='skuAndSize__20823134:M 10 / W 11.5']")).click();
+//		driver.findElement(By.cssSelector("id(\'buyTools\')/div[2]/button[1]")).click();
+		// <p class="product-display-name nsg-font-family--base edf-font-size--regular nsg-text--dark-grey">Nike Air VaporMax Flyknit</p>
+		//<img src="https://images.nike.com/is/image/DotCom/pwp_sheet2?$NIKE_PWPx3$&amp;$img0=904732_001">
+		////*[@id="exp-gridwall-wrapper"]/div[2]/div[2]/div[2]/div[1]/div[1]/div[7]/div/div/div[1]/div/a/img
+		// <p class="product-display-name nsg-font-family--base edf-font-size--regular nsg-text--dark-grey">NikeLab Free RN Commuter 2017 Gyakusou</p>
+		
+		/* Adding size 9.5 Free RN Commuter to cart from NikeLab page
+		driver.findElement(By.xpath("//*[@id=\'exp-gridwall-wrapper\']/div[2]/div[2]/div[2]/div[1]/div[1]/div[7]/div/div/div[1]/div/a/img")).click();
+		Thread.sleep(5000);
+
+		// //*[@id="buyTools"]/div[1]/div/label[8]
+		driver.findElement(By.xpath("//*[@id=\'buyTools\']/div[1]/div/label[8]")).click();
+		
+		// //*[@id="buyTools"]/div[2]/button[1]
+		
+		driver.findElement(By.xpath("//*[@id=\'buyTools\']/div[2]/button[1]")).click();
+		*/
 	}
 
 }
