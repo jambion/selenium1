1bcdc4656c418e53cf8938991624c56245d8c44e
JXA <Ambion_J@yahoo.com>
2018-01-12 14:24:17 -0800

    Radio Buttons

--- src/basics/Radio.java
+++ src/basics/Radio.java
@@ -4,16 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Spicejet {
+public class Radio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver_win32//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.spicejet.com/");
 		
-		// table[id='ctl00_mainContent_rbtnl_Trip']/tbody/tr/td/input
-		driver.findElement(By.cssSelector("table[id='ctl00_mainContent_rbtnl_Trip']>tbody>tr>td:nth-of-type(2)>input")).click();
+		driver.get("http://www.echoecho.com/htmlforms10.htm");
+		
+		int count = driver.findElements(By.cssSelector("input[name='group1']")).size();
+		System.out.println(count);
 		
 	}
 
