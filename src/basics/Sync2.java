fa8e3a9c9827dfd1252c164032f8822fa288a0bd
JXA <Ambion_J@yahoo.com>
2018-01-17 14:11:35 -0800

    Update

--- src/basics/Sync2.java
+++ src/basics/Sync2.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Sync2 {
 
@@ -14,11 +16,15 @@ public class Sync2 {
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver_win32//chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.alaskaair.com/");
 		driver.findElement(By.cssSelector("input[id='fromCity1']")).sendKeys("San Francisco, CA (SFO-San Francisco Intl.)");
 		driver.findElement(By.cssSelector("input[id='toCity1']")).sendKeys("Boston, MA");
 		driver.findElement(By.cssSelector("input[id='findFlights']")).click();
+		
+		WebDriverWait d = new WebDriverWait(driver, 20);
+		
+//		d.until(ExpectedConditions.visibilityOfElementLocated(locator))
 		driver.findElement(By.cssSelector("a[id='navAskJenn']")).click();
 	}
 
