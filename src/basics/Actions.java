027add599fcd529d7dfbd8dbe2aed3261b2a9105
JXA <Ambion_J@yahoo.com>
2018-01-17 14:20:23 -0800

    Actions

---
+++ src/basics/Actions.java
@@ -0,0 +1,18 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Actions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C://chromedriver_win32//chromedriver.exe");  
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.com");
+		
+		
+	}
+
+}
--- src/basics/Sync2.java
+++ src/basics/Sync2.java
@@ -16,13 +16,13 @@ public class Sync2 {
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver_win32//chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 		driver.get("https://www.alaskaair.com/");
 		driver.findElement(By.cssSelector("input[id='fromCity1']")).sendKeys("San Francisco, CA (SFO-San Francisco Intl.)");
 		driver.findElement(By.cssSelector("input[id='toCity1']")).sendKeys("Boston, MA");
 		driver.findElement(By.cssSelector("input[id='findFlights']")).click();
 		
-		WebDriverWait d = new WebDriverWait(driver, 20);
+//		WebDriverWait d = new WebDriverWait(driver, 20);
 		
 //		d.until(ExpectedConditions.visibilityOfElementLocated(locator))
 		driver.findElement(By.cssSelector("a[id='navAskJenn']")).click();
