1b8528352173fb191730d4ec3d602e152d4fcd9c
JXA <Ambion_J@yahoo.com>
2018-01-18 09:31:03 -0800

    Created link counter

---
+++ src/basics/LinkCount.java
@@ -0,0 +1,17 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LinkCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:/chromedriver_win32/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.ebay.com");
+	}
+
+}
