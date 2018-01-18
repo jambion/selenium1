c69d8c5cb1ec7d5e3655ec2873e44ab3518b5445
JXA <Ambion_J@yahoo.com>
2018-01-18 09:40:05 -0800

    Find total number of links for the entire page and footer

--- src/basics/LinkCount.java
+++ src/basics/LinkCount.java
@@ -1,6 +1,8 @@
 package basics;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LinkCount {
@@ -12,6 +14,15 @@ public class LinkCount {
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.ebay.com");
+		
+		// Count all of the links in the page 
+		System.out.println("Total links on the entire webpage");
+		System.out.println(driver.findElements(By.tagName("a")).size());
+		
+		// Count of links in the footer section 
+		WebElement footer = driver.findElement(By.id("hlGlobalFooter"));
+		System.out.println("Total links in the footer section: ");
+		System.out.println(footer.findElements(By.tagName("a")).size());
 	}
 
 }
