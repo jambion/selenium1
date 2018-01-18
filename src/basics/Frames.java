85427387d51f8e078c7c348a0536db1288254cfd
JXA <Ambion_J@yahoo.com>
2018-01-17 16:19:53 -0800

    Completed Frames

--- src/basics/Frames.java
+++ src/basics/Frames.java
@@ -1,10 +1,19 @@
 package basics;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Frames {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "C://chromedriver_win32//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://jqueryui.com/droppable/");
+		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[src='/resources/demos/droppable/default.html']")));
+		driver.findElement(By.id("draggable"));
+		
 	}
 
 }
